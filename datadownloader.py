4e20f9c7870c85484a768861e29f0f63da322763
PPFilip <PPFilip@users.noreply.github.com>
2019-10-21 17:30:56 +0200

    New data sources for altcoins

--- datadownloader.py
+++ datadownloader.py
@@ -6,9 +6,21 @@ urls = {
     'BTC': 'https://coinmetrics.io/newdata/btc.csv',
     'ETH': 'https://coinmetrics.io/newdata/eth.csv',
     'DOGE': 'https://coinmetrics.io/newdata/doge.csv',
+    'LTC': 'https://coinmetrics.io/newdata/ltc.csv',
+    'BCH': 'https://coinmetrics.io/newdata/bch.csv',
+    'XMR': 'https://coinmetrics.io/newdata/xmr.csv',
+    'XRP': 'https://coinmetrics.io/newdata/xrp.csv',
     'BTCUSD': 'http://www.cryptodatadownload.com/cdd/Kraken_BTCUSD_d.csv',
+    'DOGEBTC': 'http://www.cryptodatadownload.com/cdd/Tidex_DOGEBTC_d.csv',
     'ETHUSD': 'http://www.cryptodatadownload.com/cdd/Kraken_ETHUSD_d.csv',
-    'ETHBTC': 'http://www.cryptodatadownload.com/cdd/Kraken_ETHBTC_d.csv'
+    'ETHBTC': 'http://www.cryptodatadownload.com/cdd/Kraken_ETHBTC_d.csv',
+    'LTCUSD': 'http://www.cryptodatadownload.com/cdd/Kraken_LTCUSD_d.csv',
+    'LTCBTC': 'http://www.cryptodatadownload.com/cdd/Kraken_LTCBTC_d.csv',
+    'XRPUSD': 'http://www.cryptodatadownload.com/cdd/Kraken_XRPUSD_d.csv',
+    'XRPBTC': 'http://www.cryptodatadownload.com/cdd/Poloniex_XRPBTC_d.csv',
+    'BCHUSD': 'http://www.cryptodatadownload.com/cdd/Poloniex_BCHUSD_d.csv',
+    'XMRUSD': 'http://www.cryptodatadownload.com/cdd/Poloniex_XMRUSD_d.csv',
+    'XMRBTC': 'http://www.cryptodatadownload.com/cdd/Poloniex_XMRBTC_d.csv'
 }
 
 for url in urls.values():
